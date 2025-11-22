e1237d05a090f3d5bd2ece5fd8750128cfb2fe77
chams-eddine <30511267+chams-eddine@users.noreply.github.com>
2025-11-22 19:36:46 +0400

    Change PDF download link in Validation page
    Updated PDF link to a new Google Drive URL.

--- pages/3_✅_Validation.py
+++ pages/3_✅_Validation.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.image("https://i.imgur.com/SaXlnlg.png", use_column_width=True)
 st.success("+15% Revenue Uplift – Independently Verified by Muscat Energy (Oman)")
 
-pdf = "https://drive.google.com/uc?export=download&id=1hL3z1e9v8mK8z9xY7tR5uW2qP9oN6fF_"
+pdf = "https://drive.google.com/file/d/1lW6YJJaUnLJb3NyJuPW4D_tx6RnVYfTa/view?usp=drive_link"
 st.markdown(f'''
 <div style="text-align:center; margin:60px;">
     <a href="{pdf}" target="_blank">
